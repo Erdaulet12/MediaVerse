c5ac718fa23c01e9e9de68472b1cf547b9399335
Erdaulet <erdauletakilzhan83@gmail.com>
2025-06-04 21:40:16 +0500

    1-5 fixed Popular page

--- MEDIAVERSE/src/pages/Popular.jsx
+++ MEDIAVERSE/src/pages/Popular.jsx
@@ -4,7 +4,7 @@ import "../assets/css/App.css";
 import MenuIcon from "../assets/images/menu.svg";
 import FilterIcon from "../assets/images/filter.svg";
 
-const popularItems = Array.from({ length: 20 }, (_, i) => ({ id: i + 1 }));
+const popularItems = Array.from({ length: 40 }, (_, i) => ({ id: i + 1 }));
 
 const Popular = () => {
   return (
@@ -12,23 +12,24 @@ const Popular = () => {
       <div className="app">
         <Header />
         <main className="app__main">
-          <div className="popular-section-header">
-            <div className="title-with-icon">
-              <img src={MenuIcon} alt="Menu" className="icon" />
-              <h2 className="popular-title">Popular</h2>
+          <div className="popular-container">
+            <div className="popular-grid">
+              <div className="popular-section-header">
+                <h2 className="popular-title">Popular</h2>
+                <div className="filters-with-icon">
+                  <img src={MenuIcon} alt="Menu" className="icon" />
+                  <span>Popular</span>
+                  <img src={FilterIcon} alt="Filters" className="icon" />
+                  <span>Filters</span>
+                </div>
+              </div>
+              <h3 className="popular-subtitle">Most Popular</h3>
+              <div className="cards-wrapper">
+                {popularItems.map((item) => (
+                  <div key={item.id} className="popular-card" />
+                ))}
+              </div>
             </div>
-            <div className="filters-with-icon">
-              <img src={FilterIcon} alt="Filters" className="icon" />
-              <span>Filters</span>
-            </div>
-          </div>
-
-          <h3 className="popular-subtitle">Most Popular</h3>
-
-          <div className="popular-grid">
-            {popularItems.map((item) => (
-              <div key={item.id} className="popular-card" />
-            ))}
           </div>
         </main>
       </div>
